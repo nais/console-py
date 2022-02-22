a48bf1761ebdef5bb6a73ff097013ba394f2aafd
Morten Lied Johansen <morten.lied.johansen@nav.no>
2022-02-22 22:13:51 +0100

    Fix codestyle issue by simplifying module-main
    Since our main catches and logs all Exceptions before returning, there is no
    need to also do that in __main__.py.

--- console/__main__.py
+++ console/__main__.py
@@ -3,9 +3,4 @@ import sys
 from .main import main
 
 if __name__ == "__main__":
-    rc = 1
-    try:
-        rc = main()
-    except Exception as e:
-        print(f"Error: {rc}")
-    sys.exit(rc)
+    sys.exit(main())
