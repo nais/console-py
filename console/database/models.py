7d8f690bf36e0fa2723ddca03bbc4a4c3517f764
Morten Lied Johansen <morten.lied.johansen@nav.no>
2022-02-22 10:44:06 +0100

    Extend Common model
    Co-authored-by: @x10an14-nav <christian.chavez@nav.no>
    Co-authored-by: Kim Tore Jensen <kim.tore.jensen@nav.no>
    Co-authored-by: Christer Edvartsen <christer.edvartsen@nav.no>

--- console/database/models.py
+++ console/database/models.py
@@ -1,9 +1,9 @@
 import datetime
 import uuid
 
-from sqlalchemy import Column, DateTime, Text
+from sqlalchemy import Column, DateTime, Text, Table, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import declarative_mixin, declared_attr
+from sqlalchemy.orm import declarative_mixin, declared_attr, relationship
 
 from . import Base
 
@@ -17,11 +17,51 @@ class Common:
 
     id = Column(UUID, primary_key=True, default=uuid.uuid4)
     created_at = Column(DateTime, default=datetime.datetime.now)
+
+    @declared_attr
+    def created_by_id(cls):
+        return Column(UUID, ForeignKey("users.id"))
+
+    @declared_attr
+    def created_by(cls):
+        return relationship("User")
+
     updated_at = Column(DateTime, onupdate=datetime.datetime.now)
+
+    @declared_attr
+    def updated_by_id(cls):
+        return Column(UUID, ForeignKey("users.id"))
+
+    @declared_attr
+    def updated_by(cls):
+        return relationship("User")
+
     deleted_at = Column(DateTime)
+
+    @declared_attr
+    def deleted_by_id(cls):
+        return Column(UUID, ForeignKey("users.id"))
+
+    @declared_attr
+    def deleted_by(cls):
+        return relationship("User")
+
+
+user_team = Table(
+    'association', Base.metadata,
+    Column('user_id', ForeignKey('users.id'), primary_key=True),
+    Column('team_id', ForeignKey('teams.id'), primary_key=True)
+)
 
 
 class Team(Common, Base):
     slug = Column(Text, unique=True, nullable=False)
     name = Column(Text, unique=True, nullable=False)
     purpose = Column(Text)
+    users = relationship("User", secondary=user_team, back_populates="teams")
+
+
+class User(Common, Base):
+    email = Column(Text, unique=True)
+    name = Column(Text, nullable=False)
+    teams = relationship("Team", secondary=user_team, back_populates="users")
